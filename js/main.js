c098813ced588c252a0d52c2f02bf67059ba8aad
Jamie Sharpe <git@jamiesharpe.co.uk>
2018-03-14 01:09:03 +0000

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,86 @@
+
+function insertObject(jsonObj, idColumn) {
+
+    $("<div/>", {
+        "class": 'card mb-4 box-shadow text-uppercase',
+        html: `
+                <div class="card-header text-center">
+                    <h4>${jsonObj.title}</h4>
+                </div>
+                <div class="card-body text-left">
+                        <ul id="${jsonObj.id}" class="card-text" style="list-style-type:none;">
+                        </ul>
+                </div>
+        `
+    }).appendTo("#" + idColumn);
+
+    $.each(jsonObj.links, function (index, item) {
+
+        $("<li/>", {
+            html: `<a href="${item.link}">
+                        <button type="button" class= "btn btn-link" style="padding:0;">
+                            <img src="https://plus.google.com/_/favicon?domain=${item.link}" class="align-middle"/>
+                            - ${item.text}
+                        </button>
+                    </a>`
+        }).appendTo("#" + jsonObj.id);
+    });
+}
+
+
+function insertQuote() {
+
+    // Quote link -- "https://talaikis.com/api/quotes/random/"
+    var quoteLink = "http://quotes.stormconsultancy.co.uk/random.json";
+    $.getJSON(quoteLink, function (data) {
+        $("<p/>", {
+            html: `${data.quote}
+                    <br/>
+                    - <a href="${data.permalink}"> ${data.author}</a>`
+        }).appendTo("#quote");
+    });
+}
+
+
+function insertWeather() {
+
+    $("<div/>", {
+        "class": 'card mb-4 box-shadow',
+        html: `
+                <div class="card-header text-uppercase text-center">
+                    <h4>Weather</h4>
+                </div>
+                <div id="weatherCard" class="card-body card-text" style="padding:-1em;">
+                </div>
+        `
+    }).appendTo("#col0");
+
+    $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=Derby&appid=" + weatherAPIID + "&units=metric", function (data) {
+
+        var sunrise = new Date(data.sys.sunrise * 1000).toLocaleTimeString();
+        var sunset = new Date(data.sys.sunset * 1000).toLocaleTimeString();
+        $("<ul/>", {
+            "class": "list-group list-group-flush",
+            html: `<li class="list-group-item">Description: ${data.weather[0].main}<br/>${data.weather[0].description}</li>
+                    <li class="list-group-item">Cloud Cover: ${data.clouds.all}%</li>
+                    <li class="list-group-item">Visibility: ${data.visibility}m</li>
+                    <li class="list-group-item">Temperature: ${data.main.temp}°C <br/> min: ${data.main.temp_min}°C <br/> max:${data.main.temp_max}°C</li>
+                    <li class="list-group-item">Wind Speed: ${data.wind.speed} m/s <br/> Direction: ${data.wind.deg}°</li>
+                    <li class="list-group-item">Sunrise: ${sunrise} <br/> Sunset: ${sunset}</li>
+                    <li class="list-group-item">${data.name} - ${data.sys.country}</li>
+            `
+        }).appendTo("#weatherCard");
+    });
+}
+
+
+$(document).ready(function () {
+
+    insertObject(News, "col1");
+    insertObject(University, "col2");
+    insertObject(Social, "col2");
+    insertObject(Media, "col3");
+    insertObject(Utilities, "col3");
+    insertQuote();
+    insertWeather();
+});
---
+++ js/vars.js
@@ -0,0 +1,199 @@
+var News = {
+    "id": "News",
+    "title": "News",
+    "links": {
+        "BBC": {
+            "link": "http://www.bbc.com/news",
+            "text": "BBC",
+        },
+        "Verge": {
+            "link": "https://www.theverge.com/",
+            "text": "Verge"
+        },
+        "ArsTechnica": {
+
+
+            "link": "https://arstechnica.com/",
+            "text": "Ars Technica"
+        },
+        "Guardian": {
+
+            "link": "https://www.theguardian.com/uk",
+            "text": "Guardian"
+        },
+        "SlashDot": {
+
+            "link": "https://slashdot.org/",
+            "text": "Slash Dot"
+        },
+        "Wired": {
+
+            "link": "https://www.wired.co.uk/",
+            "text": "Wired"
+        },
+        "TorrentFreak": {
+
+            "link": "https://torrentfreak.com/",
+            "text": "Torrent Freak"
+        },
+        "HackerNews": {
+            "link": "https://news.ycombinator.com/",
+            "text": "Hacker News"
+        },
+        "ACMTechNews": {
+
+            "link": "http://technews.acm.org/",
+            "text": "ACM Tech News"
+        },
+        "Lobster": {
+
+            "link": "https://lobste.rs/",
+            "text": "Lobster"
+        },
+        "TechCrunch": {
+
+            "link": "https://techcrunch.com/",
+            "text": "Tech Crunch"
+        },
+        "Engadget": {
+
+            "link": "https://www.engadget.com/uk/",
+            "text": "Engadget"
+        },
+        "MentalFloss": {
+
+            "link": "http://mentalfloss.com/",
+            "text": "Mental Floss"
+        },
+        "WallStreetJournal": {
+
+            "link": "https://www.wsj.com/europe",
+            "text": "Wall Street Journal"
+        },
+        "TopTechNews": {
+
+            "link": "https://www.toptechnews.com/",
+            "text": "Top Tech News"
+        },
+        "AlJazeera": {
+
+            "link": "https://www.aljazeera.com/",
+            "text": "Al Jazeera"
+        },
+        "Reuters": {
+
+            "link": "https://uk.reuters.com/",
+            "text": "Reuters"
+        },
+        "GoogleNews": {
+
+            "link": "https://news.google.com/news/?ned=us&gl=US&hl=en",
+            "text": "Google News"
+        }
+    }
+};
+
+var Social = {
+    "id": "Social",
+    "title": "Social",
+    "links": {
+        "Twitter": {
+            "link": "https://www.twitter.com/",
+            "text": "Twitter"
+        },
+        "LinkedIn": {
+            "link": "https://www.linkedin.com/",
+            "text": "LinkedIn"
+        },
+        "Pinterest": {
+            "link": "https://www.pinterest.co.uk/",
+            "text": "Pinterest"
+        },
+        "Instagram": {
+            "link": "https://www.instagram.com/explore/",
+            "text": "Instagram"
+        }
+    }
+};
+
+var Utilities = {
+    "id": "Utilities",
+    "title": "Utilities",
+    "links": {
+        "Email": {
+            "link": "https://mail.google.com/mail/u/0/#inbox",
+            "text": "Email"
+        },
+        "Calendar": {
+            "link": "https://calendar.google.com/calendar/r",
+            "text": "Calendar"
+        },
+        "Notes / Tasks": {
+            "link": "https://keep.google.com/u/0/#home",
+            "text": "Notes / Tasks"
+        }
+    }
+
+};
+
+var University = {
+    "id": "University",
+    "title": "University",
+    "links": {
+        "Udo": {
+            "link": "https://udo.derby.ac.uk/my.policy",
+            "text": "UDo"
+        },
+        "LibGuides": {
+            "link": "https://libguides.derby.ac.uk/",
+            "text": "Lib Guides"
+        },
+        "PiktoChart": {
+            "link": "https://create.piktochart.com/dashboard",
+            "text": "Pikto Chart"
+        },
+        "SciHub": {
+            "link": "https://sci-hub.tw/",
+            "text": "Sci Hub"
+        },
+        "WolframAlpha": {
+            "link": "https://www.wolframalpha.com/",
+            "text": "Wolfram Alpha"
+        },
+        "TypingStudying": {
+            "link": "http://www.typingstudy.com/",
+            "text": "Typing Studying"
+        },
+        "TypeRacer": {
+            "link": "http://play.typeracer.com/",
+            "text": "Type Racer"
+        },
+        "Dictionary": {
+            "link": "http://www.dictionary.com/",
+            "text": "Dictionary"
+        },
+        "Thesaurus": {
+            "link": "http://www.thesaurus.com/",
+            "text": "Thesaurus"
+        }
+    }
+};
+
+var Media = {
+    "id": "Media",
+    "title": "Media",
+    "links": {
+        "Reddit": {
+            "link": "http://www.reddit.com/",
+            "text": "Reddit",
+        },
+        "Youtube": {
+            "link": "https://www.youtube.com/",
+            "text": "Youtube",
+        },
+        "whiteNoiseGenerator": {
+            "link": "https://mynoise.net/NoiseMachines/whiteNoiseGenerator.php?c=0&l=9999534300000000000000&d=0",
+            "text": "White Noise Generator",
+        }
+    }
+};
