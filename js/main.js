f2d5c497de5ab89eaa2166158ac99d329e05ce5a
Jamie Sharpe <git@jamiesharpe.co.uk>
2018-04-15 13:48:15 +0100

    Changed time to be a clock

--- js/main.js
+++ js/main.js
@@ -1,3 +1,90 @@
+var ctx = 0;
+var radius = 0;
+var rss2json = "https://api.rss2json.com/v1/api.json?rss_url=";
+var bbcWeatherDerby = [
+    "https://weather-broker-cdn.api.bbci.co.uk/en/forecast/rss/3day/2651347"
+];
+var weatherCounter = 0;
+
+
+function drawClock() {
+    drawFace(ctx, radius);
+    drawNumbers(ctx, radius);
+    drawTime(ctx, radius);
+    updateTime();
+}
+
+
+function drawFace(ctx, radius) {
+    var grad;
+    ctx.beginPath();
+    ctx.arc(0, 0, radius, 0, 2 * Math.PI);
+    ctx.fillStyle = 'white';
+    ctx.fill();
+    grad = ctx.createRadialGradient(0, 0, radius * 0.95, 0, 0, radius * 1.05);
+    grad.addColorStop(0, '#333');
+    grad.addColorStop(0.5, 'white');
+    grad.addColorStop(1, '#333');
+    ctx.strokeStyle = grad;
+    ctx.lineWidth = radius * 0.1;
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.arc(0, 0, radius * 0.1, 0, 2 * Math.PI);
+    ctx.fillStyle = '#333';
+    ctx.fill();
+}
+
+
+function drawNumbers(ctx, radius) {
+    var ang;
+    var num;
+    ctx.font = radius * 0.25 + "px arial";
+    ctx.textBaseline = "middle";
+    ctx.textAlign = "center";
+    for (num = 1; num < 13; num++) {
+        ang = num * Math.PI / 6;
+        ctx.rotate(ang);
+        ctx.translate(0, -radius * 0.85);
+        ctx.rotate(-ang);
+        ctx.fillText(num.toString(), 0, 0);
+        ctx.rotate(ang);
+        ctx.translate(0, radius * 0.85);
+        ctx.rotate(-ang);
+    }
+}
+
+
+function drawTime(ctx, radius) {
+    var now = new Date();
+    var hour = now.getHours();
+    var minute = now.getMinutes();
+    var second = now.getSeconds();
+    //hour
+    hour = hour % 12;
+    hour = (hour * Math.PI / 6) +
+        (minute * Math.PI / (6 * 60)) +
+        (second * Math.PI / (360 * 60));
+    drawHand(ctx, hour, radius * 0.5, radius * 0.07);
+    //minute
+    minute = (minute * Math.PI / 30) + (second * Math.PI / (30 * 60));
+    drawHand(ctx, minute, radius * 0.8, radius * 0.07);
+    // second
+    second = (second * Math.PI / 30);
+    drawHand(ctx, second, radius * 0.9, radius * 0.02);
+}
+
+
+function drawHand(ctx, pos, length, width) {
+    ctx.beginPath();
+    ctx.lineWidth = width;
+    ctx.lineCap = "round";
+    ctx.moveTo(0, 0);
+    ctx.rotate(pos);
+    ctx.lineTo(0, -length);
+    ctx.stroke();
+    ctx.rotate(-pos);
+}
+
 
 function insertObject(jsonObj, idColumn) {
 
@@ -21,8 +108,7 @@ function insertObject(jsonObj, idColumn) {
         var favicon = "";
         if (item.favicon == null) {
             favicon = "https://plus.google.com/_/favicon?domain=" + item.link;
-        }
-        else {
+        } else {
             favicon = item.favicon;
         }
 
@@ -61,6 +147,36 @@ function insertQuote() {
 }
 
 
+function updateTime() {
+    moment.locale("en-gb");
+    var curDay = moment().format('dddd');
+    var curDate = moment().format('MMMM Do YYYY');
+    var curTime = moment().format('H:mm:ss');
+    $("#curTime")[0].innerText = curDay + "\n" + curDate + "\n" + curTime;
+}
+
+
+function updateWeather(url) {
+    console.log('updating weather');
+
+    $.ajax({
+        type: 'GET',
+        url: rss2json + bbcWeatherDerby[0],
+        dataType: 'jsonp',
+        success: (data) => {
+            var weather = data.items[weatherCounter].title;
+            weather = weather.replace(/\(.*?\)/g, "");
+            weather = weather.replace(", ", "\n");
+            weather = weather.replace(/°C/g, "°C\n");
+            weather = weather.replace(/imum/g, "");
+            weather = weather.replace(/erature/g, "");
+            $("#weather")[0].innerText = weather;
+            weatherCounter = (weatherCounter + 1) % data.items.length;
+        }
+    });
+}
+
+
 function insertWeather() {
 
     // Inserts the current weather on the left column
@@ -70,60 +186,16 @@ function insertWeather() {
         <div class="card-header text-uppercase text-center">
         Time - Weather
         </div>
-        <div id="weatherCard" class="card-body card-text" style="padding:-1em;">
+        <div id = "weatherCard"
+        class = "card-body card-text text-center"
+        style = "padding:-1em;">
+            <canvas id="canvas" width="170" height="170" style="background-color:#343434"></canvas>
+            <h4 id="curTime"></h4>
+            <p id="weather"></p>
         </div>
         `
     }).appendTo("#col0");
-
-    moment.locale("en-gb");
-    var curTime = moment().format('llll');
-
-    $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=Derby&appid=" + weatherAPIID + "&units=metric", function (data) {
-
-        var sunrise = new Date(data.sys.sunrise * 1000).toLocaleTimeString();
-        var sunset = new Date(data.sys.sunset * 1000).toLocaleTimeString();
-        var windSpeedMph = (data.wind.speed * 2.2369).toFixed(1);
-        $("<ul/>", {
-            "class": "list-group list-group-flush",
-            html: ` <li class="list-group-item" id="curTime">${curTime}</li>
-                    <li class="list-group-item">Description: ${data.weather[0].main}<br/>${data.weather[0].description}</li>
-                    <li class="list-group-item">Cloud Cover: ${data.clouds.all}%</li>
-                    <li class="list-group-item">Visibility: ${data.visibility}m</li>
-                    <li class="list-group-item">Temperature: ${data.main.temp}°C <br/> min: ${data.main.temp_min}°C <br/> max:${data.main.temp_max}°C</li>
-                    <li class="list-group-item">Wind Speed: ${windSpeedMph} mph <br/> Direction: ${data.wind.deg}°</li>
-                    <li class="list-group-item">Sunrise: ${sunrise} <br/> Sunset: ${sunset}</li>
-                    <li class="list-group-item">${data.name} - ${data.sys.country}</li>
-            `
-        }).appendTo("#weatherCard");
-    });
 }
-
-/*
-function setupKeyBindings() {
-    Mousetrap.bind("up", function () {
-        changeFocus(1);
-    });
-
-    Mousetrap.bind("down", function () {
-        changeFocus(-1);
-    });
-}
-
-function changeFocus(a) {
-
-    console.log("going" + a);
-
-    var currentFocus = $(":focus").index(this);
-    console.log("current Focus: " + currentFocus);
-
-    var tabindexValue = currentFocus + a;
-    console.log("New Focus " + tabindexValue);
-
-    //$('*').attr('tabindex', tabindexValue).focus();
-    $('[tabindex=' + tabindexValue + ']').focus();
-
-}
-*/
 
 
 $(document).ready(function () {
@@ -136,8 +208,15 @@ $(document).ready(function () {
     insertObject(Personal, "col3");
     insertQuote();
     insertWeather();
-    //setupKeyBindings();
-    setInterval(function () {
-        $("#curTime").text(() => { })[0].innerText = moment().format('llll');
-    }, 1000)
+
+    var canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
+    radius = canvas.height / 2;
+    ctx.translate(radius, radius);
+    radius = radius * 0.90
+    drawClock();
+    updateTime();
+    updateWeather();
+    setInterval(drawClock, 500);
+    setInterval(updateWeather, 10000);
 });
